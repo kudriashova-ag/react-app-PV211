2795da0eb9077831df5159f4113cae00afbc6cba
Anastasiia Kudriashova <kudriashova.ag@gmail.com>
2024-07-24 19:38:51 +0300

    toggleTheme

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,13 +2,15 @@ import React, { useContext } from 'react';
 import { NavLink, useLocation } from 'react-router-dom';
 import  './Header.css';
 import { ThemeContext } from '../../contexts/themeContext';
+import { MoonOutlined, SunOutlined } from '@ant-design/icons';
+import { Button } from 'antd';
 
 const Header = () => {
   const location = useLocation(); 
   const {theme, toggleTheme} = useContext(ThemeContext);
 
     return (
-      <header>
+      <header className={`theme-${theme}`}>
         <nav>
           {location.pathname !== "/" && <NavLink to="/">Home</NavLink>}
           <NavLink to="todo">Todo</NavLink>
@@ -16,7 +18,11 @@ const Header = () => {
           <NavLink to="counters">Counters</NavLink>
         </nav>
 
-        {theme}
+        <Button
+          shape="circle"
+          icon={theme === "dark" ? <SunOutlined /> : <MoonOutlined />}
+          onClick={toggleTheme}
+        />
       </header>
     );
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { RouterProvider } from 'react-router-dom';
 import router from './routes/routes';
-import { ThemeContext } from './contexts/themeContext';
 import ThemeProvider from './providers/themeProvider';
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
